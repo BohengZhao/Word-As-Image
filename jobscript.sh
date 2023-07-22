eabdb8a2dee208ff4871a6ebed6c1edddbcd261f
Boheng Zhao <zhao969@gilbreth-fe00.rcac.purdue.edu>
2023-07-21 23:00:25 -0400

    Update bash script

--- jobscript.sh
+++ jobscript.sh
@@ -4,3 +4,6 @@ conda activate ambigen
 cd $SLURM_SUBMIT_DIR
 
 bash run_word_as_image.sh
+#cd code
+#python IF_pipe.py
+#python FontClassifier.py
--- run_word_as_image.sh
+++ run_word_as_image.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-USE_WANDB=0 # CHANGE IF YOU WANT WANDB
-WANDB_USER="none"
+USE_WANDB=1 # CHANGE IF YOU WANT WANDB
+WANDB_USER="zhao969"
 
-# tone_plus_conformal, tone, conformal, none, trocr
-EXPERIMENT=trocr
+# tone_plus_conformal, tone, conformal, none, trocr, font_loss, font_loss_upgrade, dual_font_loss
+EXPERIMENT=if_loss_upgrade
 
 CONCEPT="none"
 WORD=h
+TARGET=y
 fonts=(IndieFlower-Regular)
 for j in "${fonts[@]}"
 do
@@ -17,10 +18,13 @@ do
     SEED=0
     for i in "${letter_[@]}"
     do
-        echo "$i"
-        font_name=$j
-        ARGS="--experiment $EXPERIMENT --optimized_letter ${i} --seed $SEED --font ${font_name} --use_wandb ${USE_WANDB} --wandb_user ${WANDB_USER}"
-        CUDA_VISIBLE_DEVICES=0 python code/main.py $ARGS --semantic_concept "${CONCEPT}" --word "${WORD}"
+        #for angle_w_sweep in 0.6 0.5 0.4 0.3 0.2 0.1 0.05 0.01
+        #for font_weight_sweep in 0.1 0.05 0.01 0.005 0.001
+        #do 
+            echo "$i"
+            font_name=$j
+            ARGS="--experiment $EXPERIMENT --optimized_letter ${i} --seed $SEED --font ${font_name} --use_wandb ${USE_WANDB} --wandb_user ${WANDB_USER} --init_char ${i} --target_char ${TARGET}"
+            CUDA_VISIBLE_DEVICES=0 python code/main.py $ARGS --semantic_concept "${CONCEPT}" --word "${WORD}"
+        #done
     done
 done
-
